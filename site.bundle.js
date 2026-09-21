9fb9e394e8d1fdfe43fb7efdf4cb6781328704a4
WESSELKRAMER <info@wesselkramer.nl>
2026-09-21 12:06:25 +0200

    Create site.bundle.js

---
+++ site.bundle.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smoothWheel: true,
+});
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
